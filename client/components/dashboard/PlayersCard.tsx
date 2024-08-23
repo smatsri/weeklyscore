3336fe52055a216ae67290a1c956e945c514c61c
Shai <smatsri@gmail.com>
2024-08-23 13:36:49 +0300

    asd

--- client/components/dashboard/PlayersCard.tsx
+++ client/components/dashboard/PlayersCard.tsx
@@ -1,7 +1,6 @@
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -11,13 +10,28 @@ import { Button } from "@/components/ui/button";
 
 export default function PlayersCard() {
   return (
-    <Card>
+    <Card className="min-w-[300px]">
       <CardHeader>
-        <CardTitle>Players</CardTitle>
+        <CardTitle>שחקנים מובילים</CardTitle>
       </CardHeader>
-      <CardContent>list of players</CardContent>
-      <CardFooter>
-        <Button>view all</Button>
+      <CardContent>
+        <ul className="grid gap-3">
+          <li className="flex items-center justify-between">
+            <span>שי</span>
+            <span>₪250.00</span>
+          </li>
+          <li className="flex items-center justify-between">
+            <span>אבי</span>
+            <span>₪200.00</span>
+          </li>
+          <li className="flex items-center justify-between">
+            <span>אלי</span>
+            <span>₪150.00</span>
+          </li>
+        </ul>
+      </CardContent>
+      <CardFooter className="flex justify-center">
+        <Button>הצג הכל</Button>
       </CardFooter>
     </Card>
   );
