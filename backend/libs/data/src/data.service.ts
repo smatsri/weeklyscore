ceef562a0e428f170ab2bd893459b0f110faa12e
Shai <smatsri@gmail.com>
2024-08-24 11:41:49 +0300

    ewr

--- backend/libs/data/src/data.service.ts
+++ backend/libs/data/src/data.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Player } from './entities/player.entity';
 @Injectable()
 export class DataService {
-  constructor() {}
+  constructor(
+    @InjectRepository(Player)
+    private playerRepository: Repository<Player>,
+  ) {}
 
   async generateMockData() {
+    const players = await this.playerRepository.find();
+    console.debug(players);
     console.log('generate mock data');
   }
 }
